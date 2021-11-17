d6474577f5ef19f7cc5f88c290176d98d097e5bd
Abu-Sufian <abu_sufian@vk.com>
2021-11-17 14:00:05 +0300

    Homework 11 - Complete

---
+++ HW11.py
@@ -0,0 +1,32 @@
+GOODS = {'Ноутбук': 70000,
+         'Смартфон': 35000,
+         'Телевизор': 55000,
+         'Наушники': 3000,
+         'Клавиатура': 5000,
+         'Мышка': 4000}
+
+with open('goods.txt', 'w', encoding='UTF-8') as FILE:
+    for key, value in GOODS.items():
+        FILE.write(f'{key}: {value}\n')
+
+
+def add_goods_in_file(file_path: str, name: str, price: int) -> None:
+    with open(file_path, 'r', encoding='UTF-8') as file:
+        lines = {}
+        for row in file:
+            word = row.replace('\n', '').split(': ')
+            lines[word[0]] = word[1]
+
+    lines[name] = price
+    with open(file_path, 'w', encoding='UTF-8') as file:
+        for k, v in lines.items():
+            file.write(f'{k}: {v}\n')
+
+
+amount = int(input('Какое кол-во товаров Вы хотите добавить: '))
+
+for i in range(0, amount):
+    print()
+    good_name = input('Название: ')
+    good_price = int(input('Цена: '))
+    add_goods_in_file('goods.txt', good_name, good_price)
